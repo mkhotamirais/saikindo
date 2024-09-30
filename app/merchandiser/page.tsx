9ee0c162ce90886758f85d752b1f90d1e5466081
Mkhotamirais <mkhotamirais@gmail.com>
2024-10-01 04:51:43 +0700

    ok

--- app/merchandiser/page.tsx
+++ app/merchandiser/page.tsx
@@ -1,50 +1,54 @@
-import { Banner } from "@/components/banner";
-import { Container, H2 } from "@/components/wrapper";
-
-export default function MerchandiserPage() {
-  return (
-    <div>
-      <Container>
-        <Banner title="Merchandiser" image="/images/banner/storyset-event.png" />
-        <div className="flex flex-col gap-4">
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Tote Bag" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Tumbler" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Hat" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Flag" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Doll" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="ID Card" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Layard" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Mug" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-          <div className="bg-white p-4 shadow rounded">
-            <H2 className="" title="Souvenier" />
-            <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-          </div>
-        </div>
-      </Container>
-    </div>
-  );
+export default function MarchandiserPage() {
+  return <div>MarchandiserPage</div>;
 }
+
+// import { Banner } from "@/components/banner";
+// import { Container, H2 } from "@/components/wrapper";
+
+// export default function MerchandiserPage() {
+//   return (
+//     <div>
+//       <Container>
+//         <Banner title="Merchandiser" image="/images/banner/storyset-event.png" />
+//         <div className="flex flex-col gap-4">
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Tote Bag" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Tumbler" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Hat" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Flag" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Doll" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="ID Card" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Layard" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Mug" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//           <div className="bg-white p-4 shadow rounded">
+//             <H2 className="" title="Souvenier" />
+//             <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//           </div>
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// }
--- app/products-services/uniform/(components)/uniform-lainnya.tsx
+++ app/products-services/uniform/(components)/uniform-lainnya.tsx
@@ -1,42 +1,48 @@
-import { HomeClient } from "@/app/(root)/(components)/home-client";
-import { Badge } from "@/components/ui/badge";
-import { H2 } from "@/components/wrapper";
 import React from "react";
 
-const portofolioList = ["Seragam/uniform", "Jaket", "Polo", "Wearpack", "Rompi", "Denim", "shoes"];
-const infoLainnyaList = ["Contoh design", "Cara order", "Bahan", "Standart ukuran"];
-
-export default function UniformLainnya() {
-  return (
-    <div>
-      <div className="flex flex-col gap-4">
-        <div className="bg-white p-4 shadow rounded">
-          <H2 className="" title="Portofolio" />
-          <div className="flex gap-1 flex-wrap py-2">
-            {portofolioList.map((item, i) => (
-              <Badge key={i} className="bg-green-600">
-                {item}
-              </Badge>
-            ))}
-          </div>
-          <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-        </div>
-        <div className="bg-white p-4 shadow rounded">
-          <H2 title="Our Client" />
-          <HomeClient />
-        </div>
-        <div className="bg-white p-4 shadow rounded">
-          <H2 className="" title="Info Lainnya" />
-          <div className="flex gap-1 flex-wrap">
-            {infoLainnyaList.map((item, i) => (
-              <Badge key={i} className="bg-green-600">
-                {item}
-              </Badge>
-            ))}
-          </div>
-          <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
-        </div>
-      </div>
-    </div>
-  );
+export default function UniformLainnyaPage() {
+  return <div>UniformLainnyaPage</div>;
 }
+
+// import { HomeClient } from "@/app/(root)/(components)/home-client";
+// import { Badge } from "@/components/ui/badge";
+// import { H2 } from "@/components/wrapper";
+// import React from "react";
+
+// const portofolioList = ["Seragam/uniform", "Jaket", "Polo", "Wearpack", "Rompi", "Denim", "shoes"];
+// const infoLainnyaList = ["Contoh design", "Cara order", "Bahan", "Standart ukuran"];
+
+// export default function UniformLainnya() {
+//   return (
+//     <div>
+//       <div className="flex flex-col gap-4">
+//         <div className="bg-white p-4 shadow rounded">
+//           <H2 className="" title="Portofolio" />
+//           <div className="flex gap-1 flex-wrap py-2">
+//             {portofolioList.map((item, i) => (
+//               <Badge key={i} className="bg-green-600">
+//                 {item}
+//               </Badge>
+//             ))}
+//           </div>
+//           <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//         </div>
+//         <div className="bg-white p-4 shadow rounded">
+//           <H2 title="Our Client" />
+//           <HomeClient />
+//         </div>
+//         <div className="bg-white p-4 shadow rounded">
+//           <H2 className="" title="Info Lainnya" />
+//           <div className="flex gap-1 flex-wrap">
+//             {infoLainnyaList.map((item, i) => (
+//               <Badge key={i} className="bg-green-600">
+//                 {item}
+//               </Badge>
+//             ))}
+//           </div>
+//           <div className="w-full h-56 bg-gray-100 flex items-center justify-center">content</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
